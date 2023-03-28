e3f3d2f770b93f68a070d225da807e57ae26ac7e
dayulop <arielsantander28@gmail.com>
2023-03-28 12:03:24 -0300

    modificaciones
    funciona

--- src/main/java/modelo/Partido.java
+++ src/main/java/modelo/Partido.java
@@ -1,40 +1,40 @@
 package modelo;
-
+import modelo.Equipo;
 import com.opencsv.bean.CsvBindByPosition;
 
 public class Partido {
-   /* public Partido(Integer id, Equipo equipo1, Equipo equipo2, Integer goles_equipo1, Integer goles_equipo2){
+    public Partido(Integer id, Equipo equipo1, Equipo equipo2, Integer goles_equipo1, Integer goles_equipo2){
         this.id = id;
         this.equipo1 = equipo1;
         this.equipo2 = equipo2;
         this.goles_equipo1 = goles_equipo1;
         this.goles_equipo2 = goles_equipo2;
-    }*/
-    private Equipo equipoA;
-    private Equipo equipoB;
+    }
+    //private Equipo equipoA;
+    //private Equipo equipoB;
 
-    @CsvBindByPosition(position = 0)
+    //@CsvBindByPosition(position = 0)
     private Integer id;
-    @CsvBindByPosition(position = 1)
-    private String equipo1;
-    @CsvBindByPosition(position = 2)
+    //@CsvBindByPosition(position = 1)
+    private Equipo equipo1;
+    //@CsvBindByPosition(position = 2)
     private Integer goles_equipo1;
-    @CsvBindByPosition(position = 3)
+    //@CsvBindByPosition(position = 3)
     private Integer goles_equipo2;
-    @CsvBindByPosition(position = 4)
-    private String equipo2;
+    //@CsvBindByPosition(position = 4)
+    private Equipo equipo2;
 
 
     public void setId(Integer id){
         this.id = id;
     }
     public void setEquipo1(Equipo equipoA){
-        this.equipoA = equipoA;
-        this.equipo1 = equipoA.getNombre();
+        this.equipo1 = equipoA;
+        //this.equipo1 = equipoA.getNombre();
     }
     public void setEquipo2(Equipo equipoB){
-        this.equipoB = equipoB;
-        this.equipo2 = equipoB.getNombre();
+        this.equipo2 = equipoB;
+        //this.equipo2 = equipoB.getNombre();
     }
     public void setGoles_equipo1(Integer goles){
         this.goles_equipo1 = goles;
@@ -43,13 +43,13 @@ public class Partido {
         this.goles_equipo2 = goles;
     }
 
-    public String getEquipo1(){
+    /*public String getEquipo1(){
         return equipo1;
     }
     public String getEquipo2(){
 
         return equipo2;
-    }
+    }*/
     public Integer getGoles_equipo1(){
         return goles_equipo1;
     }
@@ -59,7 +59,7 @@ public class Partido {
 
     public ResultadoEnum resultado(Integer equipo){
         ResultadoEnum resultadoReal = null;
-        if(equipo == equipoA.id()) {
+        if(equipo == equipo1.id()) {
             if (goles_equipo1 > goles_equipo2) {
                 resultadoReal = ResultadoEnum.GANADOR;
             } else if (goles_equipo1 == goles_equipo2) {
@@ -69,7 +69,7 @@ public class Partido {
             }
         }
 
-        if(equipo == equipoB.id()) {
+        if(equipo == equipo2.id()) {
             if (goles_equipo2 > goles_equipo1) {
                 resultadoReal = ResultadoEnum.GANADOR;
             } else if (goles_equipo2 == goles_equipo1) {
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,19 +3,73 @@ import java.io.IOException;
 import java.io.FileReader;
 import java.util.List;
 
+import com.opencsv.CSVReader;
 import com.opencsv.bean.CsvToBeanBuilder;
 
 import modelo.Equipo;
 import modelo.Partido;
+import modelo.Pronostico;
+
 public class Main {
     public static void main(String[] args) throws IOException {
-        equipos();
         if (args.length == 0){
             System.out.println("ERROR: No ingresaste ningún archivo como argumento!");
             System.exit(88);
         }
+        String  File = "C:\\Users\\ariel\\Documents\\GitHub\\CSV\\Prueba2\\src\\main\\resources\\pronostico.csv";
+        CSVReader reader   = new CSVReader(new FileReader(args[0]));
 
-        List<Partido> partidos;
+        String[]  nextLine = new String[10];
+        String[]  temporal = new String[10];
+
+        Integer[] columna1 = new Integer[10];  //guarda el id del equipo 1 de cada fila(cada fila equivale a un partido)
+        String[]  columna2 = new String[10];   //guarda el nombre del equipo 1 de cada fila
+        Integer[] columna3 = new Integer[10];//goles equipo 1
+
+        Integer[] columna4 = new Integer[10];  //goles equipo 2
+        String[]  columna5 = new String[10];   //nombre equipo 2
+        Integer[] columna6 = new Integer[10];  //id equipo 2
+
+        String[]  columna7 = new String[10];   //descripcion equipo 1
+        String[]  columna8 = new String[10];   //descripcion equipo 2
+        //TODO usar buffereader para devolver las lineas.
+        try{
+            nextLine = reader.readNext();
+            String [] fila = new String[]{nextLine[0]};//TODO tendria que ir aumentando el argumento de nexLine(solo lee la primer fila)
+
+            for(int i=0; i< fila.length;i++){//este for recorre las filas del texto
+
+                temporal= fila[i].split(";");//guarda lo que hay en la fila separado por ";". asi se genera las columnas
+
+                columna1[i]= Integer.parseInt(temporal[0]);
+                columna2[i]= temporal[1];
+                columna3[i]= Integer.parseInt(temporal[2]);
+                columna4[i]= Integer.parseInt(temporal[3]);
+                columna5[i]= temporal[4];
+                columna6[i]= Integer.parseInt(temporal[5]);
+                columna7[i]= temporal[6];
+                columna8[i]= temporal[7];
+            }
+
+        }catch (Exception e){
+            System.out.println("algo fallo arriba  " + e);
+        }
+        Equipo equipo1  = new Equipo(columna1[0],columna2[0],columna7[0]);
+        Equipo equipo2  = new Equipo(columna6[0],columna5[0],columna8[0]);
+
+        Partido partido1= new Partido(1,equipo1,equipo2,columna3[0],columna4[0]);
+        Pronostico pronostico1= new Pronostico(1,"mariana",partido1, equipo1.id(), "ganador",1);
+        pronostico1.getData();
+
+
+
+
+
+
+
+
+        //equipos();
+        /*List<Partido> partidos;
         try{
             partidos = new CsvToBeanBuilder(new FileReader(args[0]))
                     .withType(Partido.class)
@@ -27,10 +81,10 @@ public class Main {
             }
         }catch (IOException e){
             e.printStackTrace();
-        }
+        }*/
     }
 
-    public static void equipos(){
+    /*public static void equipos(){
         Equipo argentina = new Equipo(
                 1,
                 "Argentina",
@@ -47,8 +101,8 @@ public class Main {
         partido1.setGoles_equipo1(1);
         partido1.setEquipo2(arabiaS);
         partido1.setGoles_equipo2(2);
-        partido1.setId(1);
+        partido1.setId(1);+/
 
 
-    }
+    }*/
 }
